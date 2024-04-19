669d992f9408ac785122c64b78c53aab04683441
ChrisKnowsDev <chrisharley81@gmail.com>
2024-04-19 10:22:10 -0400

    optimized build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     domains: ['utfs.io']
+  },
+  // ignore typescript and eslint build errors during build because we can run these checks separately in a github action so that we don't block our builds.
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
   }
 }
 
